09169bcf245b87c9a40a27e45c2f9398ecc6efb4
alexgit2023 <alxnst07@gmail.com>
2024-07-11 15:15:38 +0300

    time of video 1.25.00

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { Layout } from 'antd';
-import AppHeader from './components/Layout/AppHeader';
-import AppSider from './components/Layout/AppSider';
-import AppContent from './components/Layout/AppContent';
+import AppLayout from "./components/Layout/AppLayout";
+import { CryptoContextProvider } from "./context/crypto-context";
 
 export default function App() {
   return (
-    <Layout>
-      <AppHeader />
-      <Layout>
-        <AppSider />
-        <AppContent />
-      </Layout>
-    </Layout>
-  )
+    <CryptoContextProvider>
+      <AppLayout />
+    </CryptoContextProvider>
+  );
 }
